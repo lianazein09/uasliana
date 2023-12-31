7aa2f0bd3b7d7dcea124150dc5c00a7f2f65ddd4
lianazein09 <lianazeinhalim@gmail.com>
2023-12-31 18:45:55 +0700

    second push

--- webberita/settings.py
+++ webberita/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
 import pymysql
 
 pymysql.version_info = (1, 4, 6, 'final', 0)
@@ -126,9 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)    
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
